27fcb7688606dd228ece036146b38005cdcd24a4
EveNyarango <evenyarango@gmail.com>
2020-10-16 10:50:00 +0300

    add content on delivery

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#order").click(function () {
+        $("#receipt").show("slow");
+    });
+    $("#del").click(function () {
+        $("#delivery").show("slow");
+    });
+
+});
